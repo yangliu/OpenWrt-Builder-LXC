ba9b427f718d59a67c2d5be4168f8a951d9da413
Yang Liu <i@yangliu.name>
2025-05-02 13:25:08 +0800

    Update diy-part2-gateway.sh
    change default ip address and netmask for my personal use

--- diy-part2-gateway.sh
+++ diy-part2-gateway.sh
@@ -58,7 +58,8 @@ function clean_packages(){
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.4.1/g' package/base-files/files/bin/config_generate
+sed -i 's/255.255.255.0/255.255.248.0/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
